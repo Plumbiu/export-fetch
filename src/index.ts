efcabef64ca02ae9ad9847f1626f3dbfbc2ca0ba
Plumbiu <3434909403@qq.com>
2023-08-26 10:34:57 +0800

    fix: js to ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
 export default function exportFetch() {
 	return {
 		name: 'rollup-export-fetch',
-		async transform(code) {
+		async transform(code: string) {
 			const tokens = code.split('\n')
 			const urlReg = /ef\[([\w:/.?]*)\]/
 			const replaceReg = /['`"]ef\[([\w:/.?]*)\]['`"]/
-			if (!code.includes('ef['))
-				return null
+			if (!code.includes('ef[')) return null
 			for (let i = 0; i < tokens.length; i++) {
 				if (urlReg.test(tokens[i])) {
 					const url = tokens[i].match(urlReg)?.[1]
-					if (url == undefined)
-						continue
+					if (url == undefined) continue
 					try {
 						const data = await fetch(url)
 						const result = await data.json()
 						tokens[i] = tokens[i].replace(replaceReg, JSON.stringify(result))
-					}
-					catch(err) {
+					} catch (err) {
 						tokens[i] = tokens[i].replace(replaceReg, JSON.stringify(err))
 					}
 				}
 			}
 			return tokens.join(';')
-		}
+		},
 	}
 }
